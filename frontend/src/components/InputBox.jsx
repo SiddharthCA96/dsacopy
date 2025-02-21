33289c8645f7b3a35fbae0a19244da789ea7bcf1
SiddharthCA96 <ss6156852@gmail.com>
2025-02-21 23:35:28 +0530

    10

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,9 +1,16 @@
 
-export function InputBox({label, placeholder, onChange}) {
-    return <div>
+export function InputBox({ label, placeholder, onChange, type = "text" }) {
+  return (
+    <div>
       <div className="text-sm font-medium text-left py-2">
         {label}
       </div>
-      <input onChange={onChange} placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
+      <input
+        type={type}
+        onChange={onChange}
+        placeholder={placeholder}
+        className="w-full px-2 py-1 border rounded border-slate-200"
+      />
     </div>
+  );
 }
