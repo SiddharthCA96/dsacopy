145d1b639aff4f9f9839bd7273f699bb390d0c94
SiddharthCA96 <ss6156852@gmail.com>
2025-02-21 23:02:58 +0530

    6

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -21,7 +21,7 @@ router.post("/signin", signin);
 router.get("/fetchdata",fetchData);
 
 //test route
-router.post("/test", tt);
+// router.post("/test", tt);
 
 //update info route
 router.put("updateInfo", authMiddleware, updateInfo);
